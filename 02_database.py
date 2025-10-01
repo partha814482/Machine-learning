4bf9414b56978bcae22877af8821a53a922dc2d3
PARTHASARATHI BEHERA <parthasarathibehera925@gmail.com>
2025-10-01 17:49:10 +0530

    Add files via upload

---
+++ 02_database.py
@@ -0,0 +1,11 @@
+import mysql.connector
+
+conn = mysql.connector.connect(host = 'localhost',user = 'root', password = '8144')
+
+if conn.is_connected():
+    print('connection established')
+    
+mycursor = conn.cursor()
+mycursor.execute('create database internshipdb')
+print(mycursor)
+
